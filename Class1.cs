8915ed1181067fd6b40f3bef676004509406a07c
dda2018 <40380078+dda2018@users.noreply.github.com>
2022-03-14 15:52:46 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitProject
+{
+    class Class1
+    {
+        public int Property1 { get; set; }
+        public int Property22 { get; set; }
+    }
+}
---
+++ Class2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitProject
+{
+    class Class2
+    {
+        public string strProperties1 { get; set; }
+        public string strProperties2 { get; set; }  // забыли добавить комментарий
+        public string strProperties3 { get; set; }
+    }
+}
